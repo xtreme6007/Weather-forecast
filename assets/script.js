7b5f94f55c2b2b046b13b71adf9e0a9cce8075fb
xtreme6007 <prestonnichols96@gmail.com>
2020-08-04 10:44:11 -0500

    stored data from open weather maps api response into variables

--- assets/script.js
+++ assets/script.js
@@ -3,29 +3,37 @@ var cityNameEl = $("#cityName");
 var searchButton = $("#citySearch");
 var todayForecast = $("currentDayForecast")
 // when search button is clicked
-searchButton.on("click", function() {
+searchButton.on("click", function () {
+    $("#currentDayForecast").empty();
     // get name of city
     var cityName = cityNameEl.val();
     // add name of city to url 
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=f6c77a9f94d27e264229784c1325f0c5&units=imperial";
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=f6c77a9f94d27e264229784c1325f0c5&units=imperial";
     // request information from open weather map
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
-          // get tempature
-          var temp = response.main.temp ;
-          // create element for temp
-          var tempEl = $("<p>");
-          // give text to element
-          tempEl.text(temp);
-          console.log(temp);
-          // append to html
-          $("#currentDayForecast").append(tempEl);
+    }).then(function (response) {
+        // store response data from open weather maps
+        var temp = response.main.temp;
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+        var windSpeed = response.wind.speed;
+        var humidity = response.main.humidty;
+        var name = response.name;
+        var feelsLike = response.main.feels_like;
+        var weather = response.weather.main;
+        // create element for temp
+        var tempEl = $("<p>");
+        // give text to element
+        tempEl.text(temp);
+        console.log(temp);
+        // append to html
+        $("#currentDayForecast").append(tempEl);
 
-   console.log(response);
-   console.log(cityName);
-      });
+        console.log(response);
+        console.log(cityName);
+    });
 
 
 });
